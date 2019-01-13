c861c15406d09a04a1eeebb5edd27fddc5d7e0fd
Sanatan Das <sanatanonline@gmail.com>
2019-01-12 19:38:00 -0800

    tf code updated

--- src/modules/ml/classification/titanic.py
+++ src/modules/ml/classification/titanic.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
 
-# LOAD DATA FROM FILE
-
 # load the data from csv file
-df = pd.read_csv('/opt/apps/code/ml-data/train.csv')
+df = pd.read_csv('C:/view/opt/apps/git/ml-data/titanic/train.csv')
 
 # print the data
 print("Variables are:", list(df))
