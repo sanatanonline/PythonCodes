b3f80d8262a642938802dc2334845b1cf00a547f
Sanatan Das <sanatanonline@gmail.com.com>
2019-06-29 19:01:14 -0700

    code is updated

---
+++ src/modules/pyt/__list__.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+print(fruits[2])
+print(len(fruits))
+
+fruits[1] = "mango"
+print(fruits)
+
+for fruit in fruits:
+    print(fruit)
+
+if "gold" in fruits:
+    print("gold is a fruit")
+else:
+    print("gold is not a fruit")
