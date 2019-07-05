956b6841f89862681feecf2c8802f054f8214399
Sanatan Das <sanatanonline@gmail.com.com>
2019-07-05 11:03:59 -0700

    code is updated

---
+++ src/modules/pyt/__iterators__.py
@@ -0,0 +1,9 @@
+fruits = ("apple", "banana", "orange", "cherry")
+i = iter(fruits)
+
+print(type(i))
+
+print(next(i))
+print(next(i))
+print(next(i))
+print(next(i))
---
+++ src/modules/pyt/__modules__.py
@@ -0,0 +1,10 @@
+import pyt.__classes__ as cl
+
+import platform
+
+x = platform.system()
+print(x)
+
+x = dir(platform)
+print(x)
+
