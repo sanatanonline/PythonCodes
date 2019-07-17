7987cff07f6a8781b8a196db84c0bf40fd8308a9
Sanatan Das <sanatanonline@gmail.com>
2019-07-17 00:40:54 -0700

    python code updated

--- src/modules/pyt/__intro__.py
+++ src/modules/pyt/__intro__.py
@@ -1,15 +1,16 @@
-from pyhomes import eye
-
+# first python program
 print("Hello World!")
 
+# conditional logic
 if 5 > 2:
     print("five is greater than 2")
 
 # This is a comment
 print("Hello, World!")
 
-print("Hello World!") # This is a comment
+print("Hello World!")  # This is a comment
 
+# multi line comment
 """
 This is a comment
 written in 
@@ -17,14 +18,17 @@ more than just one line
 """
 print("Hello, World!")
 
+# multiple assignment
 x, y, z = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
 
+# concatenation
 x = "awesome"
 print("Python is " + x)
 
+# check the element type
 x = 1    # int
 y = 2.8  # float
 z = 1j   # complex
@@ -32,10 +36,10 @@ print(type(x))
 print(type(y))
 print(type(z))
 
-
+# constructor
 x = int(1)   # x will be 1
-y = int(2.8) # y will be 2
-z = int("3") # z will be 3
+y = int(2.8)  # y will be 2
+z = int("3")  # z will be 3
 print(x)
 print(y)
 print(z)
