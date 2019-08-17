e5eb7684b6d87d148cbb83ca938b5ea62ca37d31
Sanatan Das <sanatanonline@gmail.com.com>
2019-08-17 13:27:26 -0700

    code is updated

--- src/modules/micropyt/__json_post__.py
+++ src/modules/micropyt/__json_post__.py
@@ -3,32 +3,7 @@ from flask import request
 from flask import jsonify
 import pandas as pd
 from io import StringIO
-import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
 
-x1 = ["red", "blue", "green", "orange", "yellow"]
-y1 = [5, 2, 7, 8, 2]
-
-x2 = ["red", "blue", "green", "orange", "yellow"]
-y2 = [8, 6, 2, 5, 6]
-
-plt.bar(x1, y1, label="Bar1")
-plt.bar(x2, y2, label="Bar2", color="g")
-
-plt.title("Bar Graph")
-plt.xlabel("X-axis data")
-plt.ylabel("Y-axis data")
-
-plt.legend()
-
-# plt.show()
-fig = plt.figure()
-figdata = BytesIO()
-fig.savefig(figdata, format='png')
-figdata.seek(0)
-d = base64.b64encode(figdata.read())
-print(d)
 
 app = Flask(__name__)
 
@@ -44,8 +19,13 @@ def do_first_analysis():
     number_of_features = 12
     features = list(df)
     response = jsonify(
-        {'shape': str(df.shape), 'number_of_samples': number_of_samples, 'number_of_features': number_of_features,
-         'features': features, 'image_content': str(d)})
+        {'file_name': content['file_name'],
+         'file_size': content['file_size'],
+         'file_type': content['file_type'],
+         'shape': str(df.shape),
+         'number_of_samples': number_of_samples,
+         'number_of_features': number_of_features,
+         'features': features})
     return response
 
 
